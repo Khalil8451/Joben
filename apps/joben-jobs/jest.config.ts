725b18491b0a0c67e91c3c1e07e742aaf0c3a335
Khalil8451 <khalilguesmi17@gmail.com>
2025-10-19 14:51:46 +0100

    feat(jobs): add Jobs microservice for job engine functionality

---
+++ apps/joben-jobs/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'joben-jobs',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/joben-jobs',
+};
